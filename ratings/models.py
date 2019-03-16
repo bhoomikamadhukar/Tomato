1bac614cf4377b7625a238a6897c1f6b76fb9870
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-03-16 18:54:25 +0530

    add user, rating field to the Review table, add ratings field and modify get_skill_ratings

--- ratings/models.py
+++ ratings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phone_field import PhoneField
+from phone_field import PhoneField	
+import jsonfield
 
 class Teacher(models.Model):
 	name=models.CharField(max_length=100)
@@ -9,28 +10,24 @@ class Teacher(models.Model):
 	email=models.EmailField(null=True)
 	phone = PhoneField(blank=True, help_text='Contact phone number')
 	cover=models.ImageField(upload_to="all_covers/",default="all_covers/cover.jpg")
+	ratings = jsonfield.JSONField(default={5:0,4:0,3:0,2:0,1:0})
 
 	def __str__(self):
 		return f"{self.id}. {self.name}"
 
 	@property
 	def get_skill_ratings(self):
-		allskills = skills.objects.filter(teacher=self)
-		ratings = {}
 
-		for skill in allskills:
-			reviews = Reviews.objects.filter(skills=skill)
-			total = 0
-			count = len(reviews)
+		total = 0
+		sum_votes = 0
+		for key, value in self.ratings.items():
+			total += int(key) * int(value)
+			sum_votes += int(value)
 
-			for review in reviews:
-				total += review.ratings
+		if sum_votes == 0:
+			return 0
 
-			total = total / count
-
-			ratings[skill.skill] = total
-
-		return ratings 
+		return total/sum_votes
 
 class skills(models.Model):
 	teacher= models.ForeignKey(Teacher,on_delete=models.CASCADE)
@@ -40,11 +37,13 @@ class skills(models.Model):
 		return f"{self.id}. {self.skill}"
 
 class Reviews(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 	skills = models.ForeignKey(skills,on_delete=models.CASCADE,null=True)
 	teacher=models.ForeignKey(Teacher,on_delete=models.CASCADE)
-	ratings=models.IntegerField()
+	rating = models.FloatField(default=0.0, null=True, blank=True)
+	
 	comments=models.CharField(max_length=100,blank=True,null=True)
 
 
 	def __str__(self):
-		return f" {self.ratings}"
+		return f" {self.teacher}|{self.rating}"
